584b59bbc15456d94134a32cd5744cb6b8d4b672
Giuseppe <giuseppedalia.95@gmail.com>
2025-07-04 16:53:22 +0200

    Initial commit

---
+++ src/main/java/com/gym/weight/GymWeightApplication.java
@@ -0,0 +1,13 @@
+package com.gym.weight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWeightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymWeightApplication.class, args);
+	}
+
+}
